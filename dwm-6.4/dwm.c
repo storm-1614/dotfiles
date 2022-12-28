297e053960bb9f201515ad3f130cb3f0c9157408
风云雾 <storm1614@outlook.com>
2022-12-28 14:23:23 +0800

    dwm添加真正的全屏支持

--- dwm-6.4/dwm.c
+++ dwm-6.4/dwm.c
@@ -262,6 +262,7 @@ static void tile(Monitor *m);
 static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
 static void togglescratch(const Arg *arg);
+static void togglefullscr(const Arg *arg);
 static void toggletag(const Arg *arg);
 static void toggleview(const Arg *arg);
 static void togglewin(const Arg *arg);
@@ -2299,6 +2300,13 @@ togglescratch(const Arg *arg)
 		}
 	} else
 		spawn(arg);
+}
+
+void
+togglefullscr(const Arg *arg)
+{
+  if(selmon->sel)
+    setfullscreen(selmon->sel, !selmon->sel->isfullscreen);
 }
 
 void
