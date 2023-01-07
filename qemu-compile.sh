3ecd7f5da87b65f850db83953554010c8ae106f8
风云雾 <storm1614@outlook.com>
2023-01-08 07:38:40 +0800

    qemu编译参数

---
+++ qemu-compile.sh
@@ -0,0 +1,14 @@
+./configure --prefix=/usr \
+        --enable-kvm \
+        --disable-xen \
+        --enable-libusb \
+        --enable-debug-info \
+        --enable-debug \
+        --enable-sdl \
+        --enable-vhost-net \
+        --disable-debug-tcg \
+        --enable-opengl \
+        --enable-spice \
+        --enable-gtk \
+        --target-list=x86_64-softmmu\
+
