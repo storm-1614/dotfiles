e34f7313731eab5256332f7f4a1dff7c54bddc78
storm-1614 <storm1614@outlook.com>
2023-07-02 12:46:18 +0800

    clean bin files

--- bin/trayer.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-killall trayer
-if [ $? != 0 ];then
-		trayer --edge top --widthtype pixel --height 40 --transparent 30 --tint 10 --width 300
-fi
--- bin/wallpaper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-while true;do
-        feh --bg-fill --randomize ~/Pictures/wallpapers
-    sleep 2m
-#	python /data/fy4a/fy4a.py
-#	sleep 30m
-done
-
